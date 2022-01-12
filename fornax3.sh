be0671b4d868e2e3c85cdace99096df13e52eef1
Click2Cloud-Prajwal <95343388+click2cloud-prajwal@users.noreply.github.com>
2022-01-12 12:18:24 +0530

    Update fornax3.sh

--- fornax3.sh
+++ fornax3.sh
@@ -53,7 +53,7 @@ pushd /root/go/src/github.com
 git clone https://github.com/CentaurusInfra/fornax.git
          mv fornax kubeedge
          pushd /root/go/src/github.com/kubeedge
-	 ssh -t root@$b "echo yes | scp -r /etc/kubernetes/admin.conf  $c:/root/go/src/github.com/kubeedge"
+	 #ssh -t root@$b "echo yes | scp -r /etc/kubernetes/admin.conf  $c:/root/go/src/github.com/kubeedge"
 	 #echo yes | scp -r $b:/etc/kubeedge/certs  /etc/kubeedge
          #echo yes | scp -r $b:/etc/kubeedge/ca  /etc/kubeedge
 	 #echo yes | scp -r $b:/etc/kubernetes/admin.conf /root/go/src/github.com/kubeedge
