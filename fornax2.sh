2d44c075ab93c5b7851e9870367ed3437f120848
Click2Cloud-Prajwal <95343388+click2cloud-prajwal@users.noreply.github.com>
2022-01-12 13:15:13 +0530

    Update fornax2.sh

--- fornax2.sh
+++ fornax2.sh
@@ -82,7 +82,7 @@ git clone https://github.com/CentaurusInfra/fornax.git
          nohup _output/local/bin/edgecore --edgecluster > edgecore.logs 2>&1 &
          export KUBECONFIG=/etc/kubernetes/admin.conf
          nohup _output/local/bin/cloudcore > cloudcore.logs 2>&1 &
-	 ssh -t root@$c "mkdir -p /root/go/src/github.com"
+	 #ssh -t root@$c "mkdir -p /root/go/src/github.com"
          echo yes | scp -r /etc/kubernetes/admin.conf  $c:/root/
 	 ssh -t root@$c "mkdir -p /etc/kubeedge"
          echo yes | scp -r /etc/kubeedge/certs $c:/etc/kubeedge
